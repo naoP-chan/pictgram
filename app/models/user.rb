d70270143de911db23d389ede69f1a6b2d08ab27
”AWS_Naoya_Kimpara” <naoya.kinpara@gmail.com>
2019-09-26 15:29:10 +0000

    190927

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15}
+  validates :email, presence: true, format: { with: /\A.+@.+\..+\z/}
+  # 良く使われるformatは以下　/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :password, length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/}
+  
+  has_secure_password
+end
