d643bc2178c7c94fff840f64636b679a7d1933b6
”AWS_Naoya_Kimpara” <naoya.kinpara@gmail.com>
2019-09-30 13:31:56 +0000

    190930_rails_05

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'pages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
   
   root 'pages#index'
   get 'pages/help'
   
   resources :users
-  
-    root 'pages#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
