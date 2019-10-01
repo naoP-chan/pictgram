3c3e3bb8da62911cbfe23fdd3caaa246d1cfe1cf
”AWS_Naoya_Kimpara” <naoya.kinpara@gmail.com>
2019-10-01 13:06:22 +0000

    191001_rails05_assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   validates :password, length: { in: 8..32 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/}
   
   has_secure_password
+  
+  has_many :topics
 end
